d99d356d70afbb874e3046d15f27625256f847c9
Lilian Onyuka <Lilian.Onyuka@Teradata.com>
2017-06-21 16:13:29 -0500

    updateing swapping.c

--- swapping.c
+++ swapping.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
-int main(void)
-{
-	int x = 28;
-	int y = 50;
-	printf ("x is %i\n" , x);
-	printf ("y is %i\n" , y);
 
-	//swapping....
-	int temp = x; x = y; y = temp;
+
+int main(void)
+
+{
+	
+int x = 28;
+	
+int y = 50;
+	
+printf ("x is %i\n" , x);
+	
+printf ("y is %i\n" , y);
+
+	
+//swapping....
+	
+int temp = x; 
+x = y; 
+y = temp;
+
 
 printf ("x is now %i\n" , x);
-	printf ("y is now %i\n", y);
+	
+printf ("y is now %i\n", y);
+
+
 
 }
 
